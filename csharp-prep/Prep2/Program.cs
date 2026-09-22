8e484b0031e16507929bb3367e38366465a8213c
Molli <molli.s.nelson@gmail.com>
2026-09-22 09:18:23 -0600

    prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        int grade;
+        Console.Write("What is your grade?");
+        grade = int.Parse(Console.ReadLine());
+
+        if (grade >= 90)
+        {
+            Console.WriteLine($"You have an A");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine($"You have a B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine($"You have a C");
+        }
+        else if(grade >= 60)
+        {
+            Console.WriteLine($"You have a D");
+        }
+        else
+        {
+            Console.WriteLine($"You have failed");
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("YOu did not pass the class. :(");
+        }
     }
 }
