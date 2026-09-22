3cf111e25f961b6690d22c1691b78dd66515d0fe
Molli <molli.s.nelson@gmail.com>
2026-09-22 09:53:48 -0600

    prep 2 + stretch

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -7,30 +7,82 @@ class Program
         int grade;
         Console.Write("What is your grade?");
         grade = int.Parse(Console.ReadLine());
+        int lastDigit = grade %10;
+        string sign;
 
         if (grade >= 90)
         {
-            Console.WriteLine($"You have an A.");
+            if (lastDigit >= 7)
+            {
+               sign = ""; 
+            }
+            else if(lastDigit < 3)
+            {
+                sign = "-";
+            }
+            else
+            {
+                sign = "";
+            }
+            Console.WriteLine($"You have an A{sign}.");
         }
         else if (grade >= 80)
         {
-            Console.WriteLine($"You have a B.");
+            if (lastDigit >= 7)
+            {
+               sign = "+"; 
+            }
+            else if(lastDigit < 3)
+            {
+                sign = "-";
+            }
+            else
+            {
+                sign = "";
+            }
+            Console.WriteLine($"You have a B{sign}.");
         }
         else if (grade >= 70)
         {
-            Console.WriteLine($"You have a C.");
+            if (lastDigit >= 7)
+            {
+               sign = "+"; 
+            }
+            else if(lastDigit < 3)
+            {
+                sign = "-";
+            }
+            else
+            {
+                sign = "";
+            }
+            Console.WriteLine($"You have a C{sign}.");
         }
         else if(grade >= 60)
         {
-            Console.WriteLine($"You have a D.");
+            if (lastDigit >= 7)
+            {
+               sign = "+"; 
+            }
+            else if(lastDigit < 3)
+            {
+                sign = "-";
+            }
+            else
+            {
+                sign = "";
+            }
+            Console.WriteLine($"You have a D{sign}.");
         }
         else
         {
+            
             Console.WriteLine($"You have an F.");
         }
 
         if (grade >= 70)
         {
+            
             Console.WriteLine("You passed the class!");
         }
         else
